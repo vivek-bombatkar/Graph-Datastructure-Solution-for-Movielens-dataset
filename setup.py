0e601d5607af0237572f2ef98730131afd0f0ef5
Vivek <bombatkar.vivek@gmail.com>
2018-10-25 20:46:16 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ml_graph',
+   version='1.0',
+   description='A ml graph module',
+   author='vivek',
+   author_email='',
+   packages=['ml_graph'],  
+   install_requires=['none'], 
+)
